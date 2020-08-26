ebed3b088b13d3d52b9923771e83fd6e6de27ddf
dainchatel <dain@mantl.com>
2020-08-26 16:28:49 -0400

    add migrations

--- src/api/sql/migrations/V1566943365_create_people_table.sql
+++ src/api/sql/migrations/V1566943365_create_people_table.sql
@@ -1,13 +1,12 @@
-/**
- * Write SQL that creates a table named 'people'
- * it should match this schema
- * id BIGSERIAL PRIMARY KEY
- * first_name string with length 256 not null
- * last_name string with length 256 not null
- * birthday date not null
- * company string with length 256
- * title string with length 256
- * created_at timestamp with timezone not null defaults to now
- * updated_at timestamp with timezone
- * deleted_at timestamp with timezone
- ***/
+CREATE TABLE people
+(
+    id BIGSERIAL PRIMARY KEY,
+    first_name varchar(256) NOT NULL,
+    last_name varchar(256) NOT NULL,
+    birthday date NOT NULL,
+    company varchar(256)    ,
+    title varchar(256),
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ
+) 
--- src/api/sql/migrations/V1566943370_create_addresses_table.sql
+++ src/api/sql/migrations/V1566943370_create_addresses_table.sql
@@ -1,14 +1,13 @@
-/**
- * Write SQL that creates a table named 'addresses'
- * it should match this schema
- * id BIGSERIAL PRIMARY KEY
- * person_id BIGINT not null Foreign Key to people.id
- * line1 string with length 256 not null
- * line2 string with length 256
- * city string with length 256 not null
- * state string with length 256 not null
- * zip string with length 256 not null
- * created_at timetsamp with timezone not null default now()
- * updated_at timetsamp with timezone
- * deleted_at timetsamp with timezone
- ***/
+CREATE TABLE addresses
+(
+    id BIGSERIAL PRIMARY KEY,
+    person_id bigint NOT NULL REFERENCES people(id),
+    line1 varchar(256) NOT NULL,
+    line2 varchar(256) NOT NULL,
+    city varchar(256) NOT NULL,
+    state varchar(256) NOT NULL,
+    zip varchar(256) NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ
+)
