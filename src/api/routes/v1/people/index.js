92f4874d3278d91e763863273b96d7da17c7da81
John Tieu <johnson.tieu@gmail.com>
2020-09-02 02:50:58 -0400

    finished

--- src/api/routes/v1/people/index.js
+++ src/api/routes/v1/people/index.js
@@ -1,6 +1,7 @@
 const statusCodes = require('../../../lib/httpStatusCodes')
 const httpErrorMessages = require('../../../lib/httpErrorMessages')
 const { database } = require('../../../lib/database')
+const moment = require('moment')
 
 module.exports = (api) => {
   /**
@@ -8,19 +9,34 @@ module.exports = (api) => {
    * Create a new person
    */
   api.post('/', async (req, res, next) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    const newPerson = req.body
+    try {
+      const returnPerson = await database('people').insert(newPerson).returning('*')
+      res.status(statusCodes.OK).json(returnPerson[0])
+    } catch (error) {
+      next(error)
+    }
   })
+
 
   /**
    * GET /v1/people/:personID
    * Retrieve a person by their ID
    */
   api.get('/:personID', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    try {
+      const person = await database('people').where({
+        id: req.params.personID,
+      }).select('*')
+      if (person.length === 0) {
+        return res.status(statusCodes.NotFound).json({})
+      }
+      res
+        .status(statusCodes.OK)
+        .json(person[0])
+    } catch (error) {
+      next(error)
+    }
   })
 
   /**
@@ -28,9 +44,14 @@ module.exports = (api) => {
    * Retrieve a list of people
    */
   api.get('/', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    try {
+      const people = await database('people')
+      res
+        .status(200)
+        .json(people)
+    } catch (error) {
+      next(error)
+    }
   })
 
   /**
@@ -45,9 +66,16 @@ module.exports = (api) => {
    * Create a new address belonging to a person
    **/
   api.post('/:personID/addresses', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    const address = req.body
+    address.person_id = req.params.personID
+    try {
+      const returnAddress = await database('addresses').insert(address).returning('*')
+      res
+        .status(statusCodes.OK)
+        .json(returnAddress[0])
+    } catch (error) {
+      next(error)
+    }
   })
 
   /**
@@ -55,9 +83,19 @@ module.exports = (api) => {
    * Retrieve an address by it's addressID and personID
    **/
   api.get('/:personID/addresses/:addressID', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    const personId = req.params.personID
+    const addressId = req.params.addressID
+    try {
+      const address = await database('addresses').select('*').where({
+        person_id: personId,
+        id: addressId,
+      }).whereNull('deleted_at')
+      res
+        .status(statusCodes.OK)
+        .json(address[0])
+    } catch (error) {
+      res.status(statusCodes.NotFound).json({})
+    }
   })
 
   /**
@@ -65,9 +103,15 @@ module.exports = (api) => {
    * List all addresses belonging to a personID
    **/
   api.get('/:personID/addresses', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    const personId = req.params.personID
+    try {
+      const addresses = await database('addresses').select('*').where({
+        person_id: personId,
+      }).whereNull('deleted_at')
+      res.status(200).json(addresses)
+    } catch (error) {
+      next(error)
+    }
   })
 
   /**
@@ -78,8 +122,25 @@ module.exports = (api) => {
    * Update the previous GET endpoints to omit rows where deleted_at is not null
    **/
   api.delete('/:personID/addresses/:addressID', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    const personId = req.params.personID
+    const addressId = req.params.addressID
+    try {
+      await database('addresses')
+        .where({
+          person_id: personId,
+          id: addressId
+        })
+        .update({
+          deleted_at: moment().toISOString()
+        })
+      const address = await database('addresses').select('*').where({
+        person_id: personId,
+        id: addressId,
+      })
+      res.status(202).json(address[0])
+    } catch (error) {
+      next(error)
+    }
+
   })
 }
--- src/api/tests/database.spec.js
+++ src/api/tests/database.spec.js
@@ -1,36 +1,98 @@
 const fixtures = require('./fixtures')
-const { database } = require('../lib/database')
+const httpStatusCodes = require('../lib/httpStatusCodes')
+const { client } = require('./setup/supertestServer')
 const { expect } = require('chai')
 
-async function getFKConstraintsForTable (table) {
-  return database.first('*').from('information_schema.table_constraints').where({
-    table_name: table,
-    constraint_type: 'FOREIGN KEY',
-    table_schema: 'public'
-  }).limit(1)
+function hasPersonKeys(res) {
+  const keys = ['first_name', 'last_name', 'company', 'created_at', 'id', 'title']
+  keys.map(key => { if (!res.body[key]) throw new Error(`Response missing ${key}`) })
 }
 
-async function getSchemaForTable (table) {
-  return database.select([
-    'table_catalog as database_name',
-    'table_name as table_name',
-    'column_name as column_name',
-    'column_default as default_value',
-    'data_type as data_type',
-    'character_maximum_length as column_length'
-  ]).from('information_schema.columns').where({
-    table_name: table,
-    table_schema: 'public'
-  }).orderBy('table_name')
+function hasAddressKeys(res) {
+  const keys = ['id', 'person_id', 'line1', 'line2', 'city', 'state', 'zip', 'created_at',]
+  keys.map(key => { if (!res.body[key]) throw new Error(`Response missing ${key}`) })
 }
 
-describe('Database schema', () => {
-  it('Should have a correctly structured people table in the public schema', async () => {
-    expect(await getSchemaForTable('people')).to.be.deep.equal(fixtures.peopleSchema)
+describe('People API', () => {
+  it('POST /v1/people should create a new person', async () => {
+    await client
+      .post('/v1/people')
+      .send(fixtures.firstPerson)
+      .expect(httpStatusCodes.OK)
+      .expect(hasPersonKeys)
+      .then(resp => {
+        fixtures.firstPerson = resp.body
+      })
   })
 
-  it('Should have a correctly structured addresses table in the public schema', async () => {
-    expect(await getSchemaForTable('addresses')).to.be.deep.equal(fixtures.addressesSchema.structure)
-    expect(await getFKConstraintsForTable('addresses')).to.be.deep.equal(fixtures.addressesSchema.foreignKey)
+  it('GET /v1/people/:personID should return a 200 with an object of the person with that id', async () => {
+    await client
+      .get(`/v1/people/${fixtures.firstPerson.id}`)
+      .expect(httpStatusCodes.OK, fixtures.firstPerson)
+  })
+
+  it('GET /v1/people/:personID should return a 404 when an incorrect id is used', async () => {
+    await client
+      .get(`/v1/people/99999999`)
+      .expect(httpStatusCodes.NotFound)
+  })
+
+  it('GET /v1/people should return a 200 with an array of people objects', async () => {
+    await client
+      .get('/v1/people')
+      .expect('Content-Type', fixtures.contentTypes.json)
+      .expect(httpStatusCodes.OK)
+      .then(resp => {
+        expect(resp.body).to.have.lengthOf.above(0)
+
+      })
+  })
+
+  /**
+   * Do not modify above this line (use them as a reference point)
+   * Do not modify beyond this point until you have reached
+   * TDD / BDD Mocha.js / Chai.js
+   * ======================================================
+   * ======================================================
+   */
+
+  it('POST /v1/people/:personID/addresses should create a new address', async () => {
+    await client
+      .post(`/v1/people/${fixtures.firstPerson.id}/addresses`)
+      .send(fixtures.newAddress)
+      .expect('Content-Type', fixtures.contentTypes.json)
+      .expect(httpStatusCodes.OK)
+      .expect(hasAddressKeys)
+      .then((response) => {
+        expect(response.body).to.have.keys('id', 'person_id', 'line1', 'line2', 'city', 'state', 'zip', 'created_at', 'updated_at', 'deleted_at')
+        fixtures.newAddress = response.body
+      })
+  })
+  it('GET /v1/people/:personID/addresses/:addressID should return an address by its id and its person_id', async () => {
+    await client
+      .get(`/v1/people/${fixtures.firstPerson.id}/addresses/${fixtures.newAddress.id}`)
+      .then((response) => {
+        expect(response.body.person_id).to.equal(fixtures.newAddress.person_id)
+        expect(response.body.id).to.equal(fixtures.newAddress.id)
+      })
+  })
+
+  it('GET /v1/people/:personID/addresses should return a list of addresses belonging to the person by that id', async () => {
+    await client
+      .get(`/v1/people/${fixtures.firstPerson.id}/addresses`)
+      .expect(httpStatusCodes.OK)
+      .expect('Content-Type', fixtures.contentTypes.json)
+      .then((response) => {
+        expect(response.body.length).to.equal(1)
+      })
+  })
+
+  // BONUS!!!
+  it('DELETE /v1/people/:personID/addresses/:addressID should delete an address by its id (BONUS)', async () => {
+    await client
+      .delete(`/v1/people/${fixtures.firstPerson.id}/addresses/${fixtures.newAddress.id}`)
+      .then((response) => {
+        expect(response.body.deleted_at).to.not.equal(null)
+      })
   })
 })
